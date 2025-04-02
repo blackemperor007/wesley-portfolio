e785218d62fcf6bdd35b8e4e4e914a4e14e90f05
Wesley Pagnol <emperorb007@gmail.com>
2025-04-02 14:58:23 +0200

    Mise à jour

--- app/works/page.jsx
+++ app/works/page.jsx
@@ -1,43 +1,23 @@
 import React from "react";
+import { FaGithub, FaLink } from "react-icons/fa6";
+
 
 const Works = () => {
     const worksData = [
         {
-            image : "./img/pro01.png",
+            image: "./img/pro01.png",
             name: "Cash.Track App",
-            date: "18 Aug,2025",
-            category: "NextJs App"
+            category: "Web App with NextJs",
+            path: "#",
+            linkGit: "#"
         },
         {
-            image : "./img/pro02.png",
-            name: "Electricals Shop",
-            date: "18 Aug,2025",
-            category: "Website"
-        },
-        {
-            image : "./img/pro03.png",
-            name: "Pj Coconuts",
-            date: "18 Aug,2025",
-            category: "Web App"
-        },
-        {
-            image : "./img/pro04.png",
-            name: "Technofolio",
-            date: "18 Aug,2025",
-            category: "Web App"
-        },
-        {
-            image : "./img/pro05.png",
-            name: "Portfolio Emily",
-            date: "18 Aug,2025",
-            category: "Web App"
-        },
-        {
-            image : "./img/pro06.png",
-            name: "Dictionary App",
-            date: "18 Aug,2025",
-            category: "Web App"
-        },
+            image: "./img/pro02.png",
+            name: "SchoolLama",
+            category: "Web App with NextJs",
+            path: "#",
+            linkGit: "https://github.com/blackemperor007/next-dashboard-ui"
+        }
     ]
 
     return (
@@ -58,9 +38,26 @@ const Works = () => {
                                         <h3 className="font-bold text-xl"> {works.name} </h3>
                                         <p className="text-sm text-gray-400"> {works.category} </p>
                                     </div>
-                                    <div className="flex-1">
-                                        <p className="text-gray-400 text-right"> {works.date} </p>
-                                    </div>
+                                </div>
+
+                                <div className="flex gap-4 items-center">
+
+                                    <a href={works.path}
+                                        className="linear flex flex-row items-center rounded-xl bg-amber-500 px-5 py-3 text-base font-medium text-white transition duration-200 hover:bg-amber-600 active:bg-amber-700"
+                                        data-ripple-light
+                                    >
+                                        <FaLink className="mr-2 fill-white" height="16" width="16" />
+
+                                        Visiter le site
+                                    </a>
+                                    <a href={works.linkGit}
+                                        className="linear flex flex-row items-center rounded-xl bg-navy-700 px-4 py-3 text-base font-medium text-white transition duration-200 hover:bg-navy-800 active:bg-navy-900 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 dark:active:bg-white/30"
+                                        data-ripple-light
+                                    >
+                                        <FaGithub className="mr-2 fill-white" height="16" width="16" />
+                                        Dépôt GitHub
+                                    </a>
+
                                 </div>
                             </div>
                         )
