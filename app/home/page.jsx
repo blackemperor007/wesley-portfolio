83e2102cea93c8cfc39b1759d713ef11f7fcd2c3
Wesley Pagnol <emperorb007@gmail.com>
2025-04-02 16:25:26 +0200

    Homepage initialisation

---
+++ app/home/page.jsx
@@ -0,0 +1,14 @@
+import Footer from '@/components/ui/Footer'
+import Hero from '@/components/ui/Hero'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className=''>
+      HomePage
+    </div>
+    
+  )
+}
+
+export default HomePage
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,35 @@
-import Footer from '@/components/ui/Footer'
-import Hero from '@/components/ui/Hero'
 import React from 'react'
+import HomePage from './home/page'
+import Hero from '@/components/ui/Hero'
+import Footer from '@/components/ui/Footer'
 
 const Home = () => {
   return (
-    <div className='w-full min-h-screen bg-bgcolor bg-grid-white/[0.1] relative flex items-center justify-center'>
-      <div className='absolute pointer-events-none inset-0 flex items-center justify-center bg-bgcolor [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
-      <Hero/>
+    // <div className='w-full min-h-screen bg-bgcolor bg-grid-white/[0.1] relative flex items-center justify-center'>
+    //   <div className='absolute pointer-events-none inset-0 flex items-center justify-center bg-bgcolor [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'>
+    //   </div>
+    //   <Hero/>
+    // </div>
+
+    <div className="w-full min-h-screen bg-bgcolor bg-grid-white/[0.1] relative flex flex-col">
+      {/* Section Hero */}
+      <div className="flex items-center justify-center h-screen">
+        <Hero />
+      </div>
+
+      {/* Section HomePage */}
+      <div className="flex-grow flex items-center justify-center">
+        <HomePage />
+      </div>
+
+      {/* Footer */}
+      <footer className="text-white text-center py-4 bg-white dark:bg-gray-900">
+        <Footer/>
+      </footer>
+
+      {/* Effet de masque */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-bgcolor [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]">
+      </div>
     </div>
   )
 }
